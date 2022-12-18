df9cbf1fe3613a3f852b04669d2fc71b22d922cc
0xtulaoda <coderaxin@gmail.com>
2022-12-18 17:42:28 +0800

    package-lock.json

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,29 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import BLOG from '@/blog.config';
-import CJK from '@/lib/cjk';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import BLOG from '@/blog.config'
+import CJK from '@/lib/cjk'
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+  static async getInitialProps (ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
 
-  render() {
+  render () {
     return (
       <Html lang={BLOG.lang} className={BLOG.appearance === 'dark' ? 'dark' : undefined}>
         <Head>
-          {BLOG.font && BLOG.font === 'serif' ? (
+          {BLOG.font && BLOG.font === 'serif'
+            ? (
             <>
               <link rel="preload" href="/fonts/SourceSerif.var.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
               <link rel="preload" href="/fonts/SourceSerif-Italic.var.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
             </>
-          ) : (
+              )
+            : (
             <>
               <link rel="preload" href="/fonts/IBMPlexSansVar-Roman.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
               <link rel="preload" href="/fonts/IBMPlexSansVar-Italic.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
             </>
-          )}
+              )}
 
           {['zh', 'ja', 'ko'].includes(BLOG.lang.slice(0, 2).toLocaleLowerCase()) && (
             <>
@@ -53,22 +55,24 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" sizes="192x192" href="/apple-touch-icon.png"></link>
           <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="/feed"></link>
-          {BLOG.appearance === 'auto' ? (
+          {BLOG.appearance === 'auto'
+            ? (
             <>
               <meta name="theme-color" content={BLOG.lightBackground} media="(prefers-color-scheme: light)" />
               <meta name="theme-color" content={BLOG.darkBackground} media="(prefers-color-scheme: dark)" />
             </>
-          ) : (
+              )
+            : (
             <meta name="theme-color" content={BLOG.appearance === 'dark' ? BLOG.darkBackground : BLOG.lightBackground} />
-          )}
+              )}
         </Head>
         <body className="bg-day dark:bg-night">
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
